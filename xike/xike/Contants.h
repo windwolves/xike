7a31df1d6863b1065f4bde36f52a9d3a00970f30
Leading Chen <CRJ@LeadingdeMacBook-Pro.local>
2014-11-20 17:07:07 +0800

    change version number

--- xike/xike/Contants.h
+++ xike/xike/Contants.h
@@ -9,7 +9,7 @@
 #ifndef xike_Contants_h
 #define xike_Contants_h
 
-#define app_version 1.11f
+#define app_version 1.10f
 #define HOST @"http://121.40.139.180:8081"
 #define WXAppid @"wxca9958c9b1340c67"
 #define WXAppSecect @"da1c1b7d4c36be3f5bdcb8be017c5911"
